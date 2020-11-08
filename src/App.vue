98101de0ec525cf5f6b9a94978bb0053208f54b1
Musafeer <firdausmamun98@gmail.com>
2020-11-08 22:19:19 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <Leaderboard />
+</div>
+</template>
+
+<script>
+import Leaderboard from "./components/Leaderboard";
+export default {
+    name: "App",
+    components: {
+        Leaderboard,
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
